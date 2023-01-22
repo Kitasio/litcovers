0c745c0beb9378dd3052da2285f114cd1bd4cba3
Kitasio <kit.plotits@gmail.com>
2023-01-22 14:34:09 +0200

    prompt links fix

--- lib/litcovers_web/live/prompt_live/show.ex
+++ lib/litcovers_web/live/prompt_live/show.ex
@@ -4,7 +4,8 @@ defmodule LitcoversWeb.PromptLive.Show do
   alias Litcovers.Metadata
 
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(%{"locale" => locale}, _session, socket) do
+    socket = assign(socket, :locale, locale)
     {:ok, socket}
   end
 
