9b3ffeb11daa3902d633de1f4f6a0281a6f7f503
Kitasio <kit.plotits@gmail.com>
2023-01-26 09:04:00 +0200

    removed unused actions

--- lib/litcovers_web/live/image_live/index.ex
+++ lib/litcovers_web/live/image_live/index.ex
@@ -2,34 +2,10 @@ defmodule LitcoversWeb.ImageLive.Index do
   use LitcoversWeb, :live_view
 
   alias Litcovers.Media
-  alias Litcovers.Media.Image
 
   @impl true
   def mount(%{"locale" => locale}, _session, socket) do
     {:ok, assign(socket, images: list_images(socket.assigns.current_user), locale: locale)}
-  end
-
-  @impl true
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Image")
-    |> assign(:image, Media.get_image!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Image")
-    |> assign(:image, %Image{})
-  end
-
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Listing Images")
-    |> assign(:image, nil)
   end
 
   @impl true
