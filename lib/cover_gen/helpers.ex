643a5d88efe6822fd193b3be846f8374635d2ca6
Kitasio <kit.plotits@gmail.com>
2023-02-02 18:40:13 +0200

    prompt helpers are more specific

--- lib/cover_gen/helpers.ex
+++ lib/cover_gen/helpers.ex
@@ -1,16 +1,28 @@
 defmodule CoverGen.Helpers do
   # alias Litcovers.Character
 
-  def create_prompt(idea_prompt, style_prompt, _gender, :portrait) do
-    "#{random_portrait()}, #{idea_prompt}, #{style_prompt}"
+  def create_prompt(idea_prompt, style_prompt, gender, :portrait) do
+    "#{random_portrait(gender)}, #{idea_prompt}, #{style_prompt}"
   end
 
   def create_prompt(idea_prompt, style_prompt, _gender, :setting) do
     "#{idea_prompt}, #{style_prompt}"
   end
 
-  defp random_portrait do
-    ["A cjw side profile portrait", "A cjw close up portrait", "a cjw symmetrical face portrait"]
+  defp random_portrait(gender) do
+    ["A cjw side profile portrait of a #{gender_to_naming(gender)}",
+      "A cjw close up portrait of a #{gender_to_naming(gender)}",
+      "A cjw symmetrical face portrait of a #{gender_to_naming(gender)}"]
     |> Enum.random()
+  end
+
+  defp gender_to_naming(gender) do
+    case gender do
+      "male" ->
+        "man"
+      
+      "female" ->
+        "woman"
+    end
   end
 end
