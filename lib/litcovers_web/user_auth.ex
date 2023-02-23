8b0a3e383cb4645c26bccddceab56f6a8a029af7
Kitasio <kit.plotits@gmail.com>
2023-02-23 17:55:01 +0200

    try it button redirect to log in

--- lib/litcovers_web/user_auth.ex
+++ lib/litcovers_web/user_auth.ex
@@ -256,9 +256,8 @@ defmodule LitcoversWeb.UserAuth do
       conn
     else
       conn
-      |> put_flash(:error, "You must log in to access this page.")
       |> maybe_store_return_to()
-      |> redirect(to: ~p"/")
+      |> redirect(to: ~p"/#{conn.assigns.locale}/users/log_in")
       |> halt()
     end
   end
