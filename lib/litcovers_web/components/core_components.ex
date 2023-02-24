325047037df752ba549908462b235e91332624ce
Kitasio <kit.plotits@gmail.com>
2023-02-24 13:18:38 +0200

    flash msgs redesign

--- lib/litcovers_web/components/core_components.ex
+++ lib/litcovers_web/components/core_components.ex
@@ -150,26 +150,16 @@ defmodule LitcoversWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed hidden top-2 right-2 w-80 sm:w-96 z-50 rounded-lg p-3 shadow-md shadow-zinc-900/5 ring-1",
-        @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
-        @kind == :error && "bg-rose-50 p-3 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
+        "fixed cursor-pointer hidden top-5 right-5 w-80 sm:w-96 z-50 rounded-lg p-5 shadow-md shadow-zinc-900/5 ring-1",
+        "bg-sec ring-stroke-main"
       ]}
       {@rest}
     >
-      <p :if={@title} class="flex items-center gap-1.5 text-[0.8125rem] font-semibold leading-6">
-        <Heroicons.information_circle :if={@kind == :info} mini class="h-4 w-4" />
-        <Heroicons.exclamation_circle :if={@kind == :error} mini class="h-4 w-4" />
-        <%= @title %>
-      </p>
-      <p class="mt-2 text-[0.8125rem] leading-5"><%= msg %></p>
-      <button
-        :if={@close}
-        type="button"
-        class="group absolute top-2 right-1 p-2"
-        aria-label={gettext("close")}
-      >
-        <Heroicons.x_mark solid class="h-5 w-5 stroke-current opacity-40 group-hover:opacity-70" />
-      </button>
+      <div class="flex justify-between items-center">
+        <p class="text-[0.8125rem] leading-5"><%= msg %></p>
+        <Heroicons.check_circle :if={@kind == :info} class="h-6 w-6 stroke-green-300 shrink-0" />
+        <Heroicons.exclamation_triangle :if={@kind == :error} class="h-6 w-6 stroke-red-300 shrink-0" />
+      </div>
     </div>
     """
   end
